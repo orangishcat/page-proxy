157282458d1013b33575b9b4b108f091955633eb
orangishcat <fdsnail2@gmail.com>
2026-02-20 17:45:48 -0800

    docs: make logo link go back to homepage

--- docs-site/docusaurus.config.ts
+++ docs-site/docusaurus.config.ts
@@ -145,6 +145,7 @@ const config: Config = {
       logo: {
         alt: "Page Proxy",
         src: "img/logo_filled.png",
+        href: "https://orangishcat.github.io",
       },
       items: [
         {
