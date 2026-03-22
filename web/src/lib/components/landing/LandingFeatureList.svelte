688e20a372d445b5b82b1f0554154f1c4831e7cf
orangishcat <fdsnail2@gmail.com>
2026-03-21 23:39:43 -0700

    fix(web): landing page element positions and styling

--- web/src/lib/components/landing/LandingFeatureList.svelte
+++ web/src/lib/components/landing/LandingFeatureList.svelte
@@ -7,10 +7,8 @@
 <section aria-labelledby="feature-list-heading">
   <h2 id="feature-list-heading" class="sr-only">Feature list</h2>
 
-  <div class="bg-[#f3f4f3] dark:bg-[#2a2a25]">
-    <div
-      class="mx-auto w-full max-w-7xl px-6 py-24 sm:px-10 lg:grid lg:grid-cols-2 lg:items-center lg:gap-20 lg:px-16 lg:py-28"
-    >
+  <div class="bg-[#f3f4f3] dark:bg-[#2a2a25] h-140 flex justify-center place-items-center w-full">
+    <div class="mx-auto w-full max-w-7xl grid grid-cols-2 items-center gap-20">
       <div class="max-w-2xl space-y-5">
         <h3 class="text-heading text-gray-950 dark:text-white">More designing, less tinkering.</h3>
         <p class="text-xl leading-relaxed text-gray-600 dark:text-gray-300">
@@ -23,27 +21,23 @@
     </div>
   </div>
 
-  <div class="bg-[#ededec] dark:bg-[#252520]">
-    <div
-      class="mx-auto w-full max-w-7xl px-6 py-24 sm:px-10 lg:grid lg:grid-cols-2 lg:items-center lg:gap-20 lg:px-16 lg:py-28"
-    >
+  <div class="bg-[#ededec] dark:bg-[#252520] h-140 flex justify-center place-items-center w-full">
+    <div class="mx-auto w-full max-w-7xl grid grid-cols-2 items-center gap-20">
       <div>
         <LandingFeatureModuleTree />
       </div>
 
-      <div class="max-w-2xl space-y-5 lg:justify-self-end lg:text-right">
+      <div class="max-w-2xl space-y-5 justify-self-end text-right">
         <h3 class="text-heading text-gray-950 dark:text-white">Powerful scripting API.</h3>
         <p class="text-xl leading-relaxed text-gray-600 dark:text-gray-300">
-          Your desired functionality, achieved one helper method at a time.
+          Implement your desired functionality easily with our API's many helper methods.
         </p>
       </div>
     </div>
   </div>
 
-  <div class="bg-[#f3f4f3] dark:bg-[#2a2a25]">
-    <div
-      class="mx-auto w-full max-w-7xl px-6 py-24 sm:px-10 lg:grid lg:grid-cols-2 lg:items-center lg:gap-20 lg:px-16 lg:py-28"
-    >
+  <div class="bg-[#f3f4f3] dark:bg-[#2a2a25] h-140 flex justify-center place-items-center w-full">
+    <div class="mx-auto w-full max-w-7xl h-full overflow-hidden grid grid-cols-2 items-center gap-20">
       <div class="max-w-2xl space-y-5">
         <h3 class="text-heading text-gray-950 dark:text-white">Editor included.</h3>
         <p class="text-xl leading-relaxed text-gray-600 dark:text-gray-300">
@@ -53,7 +47,7 @@
         </p>
       </div>
 
-      <div class="relative">
+      <div class="relative h-full mt-40 flex justify-end">
         <LandingFeatureEditorPreview />
       </div>
     </div>
