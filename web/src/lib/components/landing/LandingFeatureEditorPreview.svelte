fca9b441692d0f84fd55f282ae5dc46f9af5889b
orangishcat <fdsnail2@gmail.com>
2026-03-21 23:01:01 -0700

    fix(web): add back border

--- web/src/lib/components/landing/LandingFeatureEditorPreview.svelte
+++ web/src/lib/components/landing/LandingFeatureEditorPreview.svelte
@@ -4,6 +4,9 @@
   const editorPreviewSrc = asset("/assets/code_editor_empty.avif");
 </script>
 
-<div class="mx-auto h-96 w-full max-w-lg overflow-hidden" aria-hidden="true">
+<div
+  class="mx-auto h-96 w-full max-w-lg overflow-hidden shadow-2xl border border-gray-300 dark:border-gray-800 rounded-sm"
+  aria-hidden="true"
+>
   <img src={editorPreviewSrc} alt="" class="block w-full select-none object-cover object-top" draggable="false" />
 </div>
