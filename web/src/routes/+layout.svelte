09e90a26b4235fb9659bd04d5ab054ecd13230df
orangishcat <fdsnail2@gmail.com>
2026-04-06 17:10:25 -0700

    fix(web): bruh ico files are not supported as the favicon

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-  import { asset } from '$app/paths';
-  import '../app.css';
+  import { asset } from "$app/paths";
+  import "../app.css";
   let { children } = $props();
 </script>
 
 <svelte:head>
-  <link rel="icon" href={asset('/favicon.ico')} />
+  <link rel="icon" href={asset("/icon.avif")} />
   <meta name="theme-color" content="#20211f" media="(prefers-color-scheme: dark)" />
   <meta name="theme-color" content="#f3f4f3" media="(prefers-color-scheme: light)" />
 </svelte:head>
